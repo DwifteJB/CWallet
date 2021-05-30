a5cf9a3f0b61238a98f31a897b83ba38915de98b
Thunder7 <christian.toska@gmail.com>
2021-05-30 21:30:49 +0200

    added db

---
+++ src/db/models/Account.js
@@ -0,0 +1,28 @@
+const {
+    DataTypes,
+    Model
+} = require('sequelize');
+const sequelize = require('../database');
+
+class Account extends Model {};
+
+Account.init({
+    username: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    publickey: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    privatekey: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+}, {
+    sequelize
+});
