64da9d905b34bcd69da0f7ab2e41306656ecf9c2
Thunder7 <christian.toska@gmail.com>
2021-05-30 22:45:19 +0200

    trans men

---
+++ src/db/models/Transaction.js
@@ -0,0 +1,40 @@
+const {
+    DataTypes,
+    Model
+} = require('sequelize');
+const sequelize = require('../database');
+
+class Transaction extends Model {};
+
+Transaction.init({
+    index: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    timestamp: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    info: {
+        sender: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        recipient: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        quantity: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        }
+    },
+    hash: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+}, {
+    sequelize
+});
+
+module.exports = Transaction;
